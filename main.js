6086a3e85913c127b496b07faf22d0a0dd8e2c69
RAJAT CHAUHAN <98510021+Rajat2614@users.noreply.github.com>
2023-03-21 21:32:39 +0530

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
     console.log(value);
-    text.style.marginTop = value * 2.5 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
     leaf.style.top = value * -1.5 + 'px';
     leaf.style.left = value * 1.5 + 'px';
     hill5.style.left = value * 1.5 + 'px';
